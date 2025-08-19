b51f9273bfaf955988eaf5eb9984b65e8b2bfcc4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-19 22:35:33 +0530

    init turborepo

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "./config"
 
 const app = express();
 const PORT = 3001;
@@ -10,7 +12,16 @@ app.post("/signup", (req,res) => {
 })
 
 app.post("/signin", (req,res) => {
+
+const userId = 1;
+
+const token = jwt.sign({
+    userId
+}, JWT_SECRET)
     
+res.json({
+    token
+})
 })
 
 app.post("/room", (req,res) => {

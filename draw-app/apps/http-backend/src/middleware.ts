fc0086ed4e58677fcc7a8f6e9d9d63fe424adc4f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-19 23:00:46 +0530

    init turborepo

--- draw-app/apps/http-backend/src/middleware.ts
+++ draw-app/apps/http-backend/src/middleware.ts
@@ -9,6 +9,7 @@ export function middleware(req: Request, res: Response, next: NextFunction) {
     const decoded = jwt.verify(token, JWT_SECRET);
 
     if (typeof decoded !== "string" && decoded.userId) {
+        // @ts-ignore
       req.userId = decoded.userId;
       next();
     } else {

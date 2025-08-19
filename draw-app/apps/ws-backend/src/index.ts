990be85c2635cc237ee464817550d3f246628d85
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-19 21:40:36 +0530

    init turborepo

--- draw-app/apps/ws-backend/src/index.ts
+++ draw-app/apps/ws-backend/src/index.ts
@@ -6,8 +6,8 @@ wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
     ws.on('message', function message(data){
-        console.log('recieved: %s', data);
+        ws.send('pong')
     });
 
-    ws.send('something')
+    
 })

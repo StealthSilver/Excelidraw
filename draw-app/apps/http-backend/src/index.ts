0693d06960977af9810241ff711c98f10dc53ea5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-19 22:32:47 +0530

    init turborepo

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -5,6 +5,19 @@ const PORT = 3001;
 
 app.use(express.json());
 
+app.post("/signup", (req,res) => {
+
+})
+
+app.post("/signin", (req,res) => {
+    
+})
+
+app.post("/room", (req,res) => {
+    
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

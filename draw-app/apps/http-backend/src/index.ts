1941a5c4a3e76c8490c3777a95a5e469834cae6b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-19 23:30:51 +0530

    init turborepo

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import {CreateUserSchema} from "@repo/common/types"
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types"
 
 const app = express();
 const PORT = 3001;
@@ -26,6 +26,14 @@ app.post("/signup", (req,res) => {
 
 app.post("/signin", (req,res) => {
 
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
+
 const userId = 1;
 
 const token = jwt.sign({
@@ -38,6 +46,15 @@ res.json({
 })
 
 app.post("/room",middleware, (req,res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
+
     res.json({
         roomId: "123"
     })

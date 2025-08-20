6408dddef0b2327a4646cc59b260db7fd3f7c0c1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-20 13:10:35 +0530

    room sessions

--- draw-app/apps/ws-backend/src/index.ts
+++ draw-app/apps/ws-backend/src/index.ts
@@ -2,33 +2,35 @@ import { WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
-const wss  = new WebSocketServer({port: 8080});
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws, request ) {
-    const url = request.url; // ws:localhost:3000?token=123123
+function checkUser(token: string): boolean {
+  const decoded = jwt.verify(token, JWT_SECRET);
 
-    if(!url){
-        return;
-    }
+  // the decoded can be a string or an object, early returning if its a string
+  if (typeof decoded == "string") {
+    return false;
+  }
 
-    const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token') || "";
-    const decoded =jwt.verify(token, JWT_SECRET);
+  if (!decoded || !decoded.userId) {
+    return false;
+  }
 
-    // the decoded can be a string or an object, early returning if its a string
-    if(typeof decoded == "string"){
-        ws.close();
-        return;
-    }
-    
-    if(! decoded || !(decoded).userId){
-        ws.close();
-        return
-    }
+  return true;
+}
 
-    ws.on('message', function message(data){
-        ws.send('pong')
-    });
+wss.on("connection", function connection(ws, request) {
+  const url = request.url; // ws:localhost:3000?token=123123
 
-    
-})
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  ws.on("message", function message(data) {
+    ws.send("pong");
+  });
+});
